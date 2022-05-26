c991c76ce281713bdfea8a7856afb185885a4635
Brandon Bermudez <brandonbermudez170499@gmail.com>
2022-05-26 14:49:23 -0600

    feat: update server config and update company dto

--- pkg/dto/company.go
+++ pkg/dto/company.go
@@ -1,8 +1,15 @@
 package dto
 
+import "proforma-backend-api/pkg/models"
+
 type CompanyInput struct {
 	Name        string `json:"name" binding:"required"`
 	Description string `json:"description" binding:"required"`
 	LogoPath    string `json:"logoPath" binding:"required"`
 	DNI         string `json:"dni" binding:"required"`
+}
+
+type CompanyListOutput struct {
+	Companies []models.Company `json:"companies"`
+	Total     uint             `json:"total"`
 }
--- pkg/server/router.go
+++ pkg/server/router.go
@@ -3,6 +3,8 @@ package server
 import (
 	"proforma-backend-api/pkg/controllers"
 	"proforma-backend-api/pkg/middlewares"
+
+	"github.com/gin-gonic/gin"
 )
 
 func (s *Server) registerRoutes() {
@@ -10,10 +12,44 @@ func (s *Server) registerRoutes() {
 
 	//initialize controllers
 	authController := controllers.NewAuthController(s.DB, s.Log)
+	userController := controllers.NewUserController(s.DB, s.Log)
+	companyController := controllers.NewCompanyController(s.DB, s.Log)
+	clientController := controllers.NewClientController(s.DB, s.Log)
 
 	//initialize group paths
 	auth := s.Engine.Group("/auth")
-	// private := s.Engine.Group("/api")
+	api := s.Engine.Group("/api")
 
 	auth.POST("/login", authController.Login())
+
+	createUserEndpoints(api, userController)
+	createClientEndpoints(api, clientController)
+	createCompanyEndpoints(api, companyController)
+}
+
+func createUserEndpoints(api *gin.RouterGroup, c controllers.IUserController) {
+	userEnpoints := api.Group("/user")
+	userEnpoints.GET("/:id", c.GetOne())
+	userEnpoints.DELETE("/:id", c.DeleteOne())
+	userEnpoints.PUT("/:id", c.Update())
+	userEnpoints.POST("/", c.CreateOne())
+	userEnpoints.GET("/list", c.GetAll())
+}
+
+func createClientEndpoints(api *gin.RouterGroup, c controllers.IClientController) {
+	clientEnpoints := api.Group("/client")
+	clientEnpoints.GET("/:id", c.GetOne())
+	clientEnpoints.DELETE("/:id", c.DeleteOne())
+	clientEnpoints.PUT("/:id", c.Update())
+	clientEnpoints.POST("/", c.CreateOne())
+	clientEnpoints.GET("/list", c.GetAll())
+}
+
+func createCompanyEndpoints(api *gin.RouterGroup, c controllers.ICompanyController) {
+	companyEnpoints := api.Group("/company")
+	companyEnpoints.GET("/:id", c.GetOne())
+	companyEnpoints.DELETE("/:id", c.DeleteOne())
+	companyEnpoints.PUT("/:id", c.Update())
+	companyEnpoints.POST("/", c.CreateOne())
+	companyEnpoints.GET("/list", c.GetAll())
 }
